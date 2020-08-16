47c2d420d8097636c349fe507d74bc9f07a5b961
despel <52083283+DESPEL@users.noreply.github.com>
2020-08-15 20:05:16 -0700

    Updates

---
+++ precache-manifest.d0231aaef0578baf1c8af935fb2ae189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb998778147d8d7fe3e58fcdbc1b6b2a",
+    "url": "/simpleToDoList/index.html"
+  },
+  {
+    "revision": "fbd6266b9af0d4d71529",
+    "url": "/simpleToDoList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d935776d61bc085bf434",
+    "url": "/simpleToDoList/static/js/2.5af0ffb9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simpleToDoList/static/js/2.5af0ffb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd6266b9af0d4d71529",
+    "url": "/simpleToDoList/static/js/main.0da734c9.chunk.js"
+  },
+  {
+    "revision": "8200d46979036fbe6ab3",
+    "url": "/simpleToDoList/static/js/runtime-main.9f1a8b48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simpleToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
